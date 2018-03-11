14e96b6146ed37a4c72373928e5f17111f4af33e
xpanda <x@pandas.ai>
2018-03-11 22:41:24 +0800

    sample test
    sample test

--- examples/HereWeGo.py
+++ examples/HereWeGo.py
@@ -1,25 +1,27 @@
 # -*- coding:utf-8 -*-
 __author__ = 'boredbird'
-import os
+import os as os
 import numpy as np
 import woe.feature_process as fp
 import woe.GridSearch as gs
 
+print('test begins here!')
+
 if __name__ == '__main__':
-    config_path = os.getcwd()+'\\config.csv'
-    data_path = os.getcwd()+'\\UCI_Credit_Card.csv'
-    feature_detail_path = os.getcwd()+'\\features_detail.csv'
-    rst_pkl_path = os.getcwd()+'\\woe_rule.pkl'
+    config_path = os.getcwd()+'/config.csv'
+    data_path = os.getcwd()+'/UCI_Credit_Card.csv'
+    feature_detail_path = os.getcwd()+'/features_detail.csv'
+    rst_pkl_path = os.getcwd()+'/woe_rule.pkl'
     # train woe rule
     feature_detail,rst = fp.process_train_woe(infile_path=data_path
                                            ,outfile_path=feature_detail_path
                                            ,rst_path=rst_pkl_path
                                            ,config_path=config_path)
     # proc woe transformation
-    woe_train_path = os.getcwd()+'\\dataset_train_woed.csv'
+    woe_train_path = os.getcwd()+'/dataset_train_woed.csv'
     fp.process_woe_trans(data_path,rst_pkl_path,woe_train_path,config_path)
     # here i take the same dataset as test dataset
-    woe_test_path = os.getcwd()+'\\dataset_test_woed.csv'
+    woe_test_path = os.getcwd()+'/dataset_test_woed.csv'
     fp.process_woe_trans(data_path,rst_pkl_path,woe_test_path,config_path)
 
     print('###TRAIN SCORECARD MODEL###')
@@ -28,9 +30,9 @@ if __name__ == '__main__':
     params['validation_path'] = woe_test_path
     params['config_path'] = config_path
 
-    params['df_coef_path'] = os.getcwd()+'\\df_model_coef_path.csv'
-    params['pic_coefpath'] = os.getcwd()+'\\model_coefpath.png'
-    params['pic_performance'] = os.getcwd()+'\\model_performance_path.png'
+    params['df_coef_path'] = os.getcwd()+'/df_model_coef_path.csv'
+    params['pic_coefpath'] = os.getcwd()+'/model_coefpath.png'
+    params['pic_performance'] = os.getcwd()+'/model_performance_path.png'
     params['pic_coefpath_title'] = 'model_coefpath'
     params['pic_performance_title'] = 'model_performance_path'
 
